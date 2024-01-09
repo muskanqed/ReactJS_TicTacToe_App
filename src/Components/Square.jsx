d7763e3b1ebc19b5f5e0267b26977b5efca46b2a
muskanqed <muskanqed@gmail.com>
2024-01-09 16:43:41 +0530

    Passing data through props

---
+++ src/Components/Square.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Square({ value }) {
+    function handleClick() {
+      console.log('clicked!');
+    }
+  
+    return (
+      <button
+        className="square"
+        onClick={handleClick}
+      >
+        {value}
+      </button>
+    );
+  }
+
+export default Square;
