62965e5a34125accb20aa8ec0295a437fc95656b
muskanqed <muskanqed@gmail.com>
2024-01-11 15:27:57 +0530

    Implementing time travel

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
+  
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
